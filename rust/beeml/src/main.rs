78c36996c87f8487060cad7f168ae12d23b8e0e4
Amos Wenger <amos@bearcove.eu>
2026-04-01 14:38:50 +0200

    Rename beeml-phonetic crate to bee-phonetic

--- rust/beeml/src/main.rs
+++ rust/beeml/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let tokens = beeml_phonetic::parse_reviewed_ipa("s t a r t");
+    let tokens = bee_phonetic::parse_reviewed_ipa("s t a r t");
     eprintln!("Starting fresh aw yiss. Parsed {} phonemes.", tokens.len());
 }
